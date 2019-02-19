e4cc31b12f092da5085b2ffb0982a0f43366dc4b
JeffreyMStanley <10723491@my.uvu.edu>
2019-02-19 11:50:08 -0700

    j

--- in_class_examples/BioLog_Challenge.R
+++ in_class_examples/BioLog_Challenge.R
@@ -14,13 +14,20 @@ hours = c("Hr_24", "Hr_48", "Hr_144")
 as.numeric(mapvalues(hours, from = c("Hr_24", "Hr_48", "Hr_144"), to = c(24,48,144)))
 
 # write a command the subsets the BioLog data to Clear_Creek samples, with dilution of 0.01, and only "Glycogen"
+df2 = df %>%
+  filter(Sample.ID == "Clear_Creek" & Dilution == 0.01 & Substrate == "Glycogen") %>%
+  gather(key = "Hours", value = "abs", c(6:8))
+
 df_long = df %>%
   gather(key = "Hours", value = "abs", c(6:8))
 
-Tween_80 = (plyr::mapvalues(df_long$Substrate, from = "Tween 80", to = "Tween_80"))
+Tween_80 = (plyr::mapvalues(df_long$Substrate, from = "Tween 80 ", to = "Tween_80"))
 df_long$Substrate = as.factor(Tween_80)
 
 # Now plot those three replicates over time.
+hours = (plyr::mapvalues(df_long$Hours, from = c("Hr_24", "Hr_48", "Hr_144"), to = c(24,48,144)))
+df_long$Hours = as.numeric(hours)
+
 hours = (plyr::mapvalues(df2$Hours, from = c("Hr_24", "Hr_48", "Hr_144"), to = c(24,48,144)))
 df2$Hours = as.numeric(hours)
 
@@ -30,16 +37,34 @@ ggplot(df2, aes(x=Hours, y=abs, color= factor(Rep))) +
 
 
 # Make a plot of Tween 80 utilization over time for ALL the samples, colored by Sample.ID
-df %>%
-  filter(Substrate == "Tween_80") %>%
-  gather(key = "Hours", value = "abs", c(6:8))
+df_Tween_80 = df_long %>%
+  filter(Substrate == "Tween_80")
+hours = (plyr::mapvalues(df_long$Hours, from = c("Hr_24", "Hr_48", "Hr_144"), to = c(24,48,144)))
+df_long0$Hours = as.factor(hours)
+
+ggplot(df_Tween_80, aes(x=Hours, y=abs, color = Sample.ID)) +
+  geom_boxplot() 
+  
 
 # Now, same plot, but combine both soils and both waters into soil and water groups and color by soil vs water
+levels(df$Sample.ID)
 
+Soil = plyr::mapvalues(df_Tween_80$Sample.ID, from = c("Soil_1", "Soil_2"), to = rep("Soil",2)) +
+   plyr::mapvalues(df_Tween_80$Sample.ID, from = c("Clear_Creek", "Waste_Water"), to = rep("Water",2))
+
+
+df_Tween_80$Sample.ID = as.factor(Soil)
+
+ggplot(df_Tween_80, aes(x=Hours, y=abs, color = Sample.ID)) +
+  geom_boxplot() 
 
 # Make a table of summary statistics: for each combination of Sample.ID and Substrate, give:
 # -- Number of observations
 # -- Mean absorbance value
+df_long_S = df_long %>%
+  group_by(Sample.ID, Substrate) %>%
+  summarise(mean_abs = mean(abs), N=n())
+
 
 # Example output ....
 
