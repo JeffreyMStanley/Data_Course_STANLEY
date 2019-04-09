f3a6f0b9eff932c5bbba268eaec97fb61ff1e863
JeffreyMStanley <10723491@my.uvu.edu>
2019-04-09 12:16:00 -0600

    :(

--- Exam_3/STANLEY_Exam_3.R
+++ Exam_3/STANLEY_Exam_3.R
@@ -26,7 +26,7 @@ p1 = ggplot(plot1.data, aes(x = year, y = lifeExp, color = continent)) +
   geom_smooth(size = 2, se = FALSE)
 
 p1 = p1 + labs(x = "Year", y = "Life Expectancy", title = "Life Expectancy Over Time \n Colored by Continent",
-               color = "Continent")
+               color = "Continent") 
 
 p2 = p1 + scale_color_manual(values = pal) + theme(plot.title = element_text(hjust = 0.5, size = 20), 
                                               legend.title = element_text(size = 16),
@@ -34,8 +34,19 @@ p2 = p1 + scale_color_manual(values = pal) + theme(plot.title = element_text(hju
                                               axis.ticks = element_line(color = NULL),
                                               panel.grid = element_line(color = "gray"),
                                               legend.key = element_rect(fill = "White"))
+                                              
+                                              
 p2
 ggsave("STANLEY_plot1.png", dpi = 300, width = 5, height = 5)
+
+
+
+ggplot(mtcars, aes(mpg,cyl))+ 
+  geom_smooth(aes(fill="Mean",level=0.095)) +
+  ggtitle(paste0(title,"\n",subheader)) +
+  scale_fill_grey(name='95% Confidence\n Interval',start=.65,end=.65) +
+  theme(plot.title = element_text(size = rel(2.0),hjust=-.1,face="bold"))
+
 # Generate data for plot 2 (run the following 5 lines only once to ensure we are all using identical data)
 set.seed(123)
 a <- data.frame( x=rnorm(20000, 10, 1.9), y=rnorm(20000, 10, 1.2) )
@@ -58,3 +69,16 @@ g1 + theme(panel.background = element_rect(fill = "White"),
            axis.text.y = element_blank(),
            axis.text.x = element_blank())
 ggsave("STANLEY_plot2.png", dpi = 300, width = 5, height = 5)
+
+fail_1 = ggplot(plot2.data, aes(x = x, y=y)) +
+  geom_bin2d()
+fail_1 + theme(panel.background = element_rect(fill = "White"),
+               axis.ticks = element_line(color = "White"),
+               panel.grid = element_line(color = "White"),
+               legend.background = element_rect(fill = "white"),
+               legend.key = element_rect(fill = "white"),
+               legend.title = element_text(color = "white"),
+               axis.text.y = element_blank(),
+               axis.text.x = element_blank())
+ggsave("STANLEY_plot2.png", dpi = 300, width = 5, height = 5)
+               
